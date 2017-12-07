585521b8b2450f823b3d7b9e9663a5c0ca2b8a03
lmhufnagel <lmhufnagel@gmail.com>
2017-12-07 15:28:05 -0500

    added trips to your trips page

--- app/serializers/seat_serializer.rb
+++ app/serializers/seat_serializer.rb
@@ -1,4 +1,17 @@
 class SeatSerializer < ActiveModel::Serializer
-  attributes :id, :available, :rider_id, :ride_id
+  attributes :id, :available, :rider_id, :ride_id, :rider, :trip
+
+
+  def rider
+    User.all.find { |r| r.id === object.rider_id }
+  end
+
+  def trip
+    Trip.all.find { |t| t.id === object.ride_id }
+  end
+
+  # def driver
+  #   User.all.find { |r| r.id === object.rider_id }
+  # end
 
 end
