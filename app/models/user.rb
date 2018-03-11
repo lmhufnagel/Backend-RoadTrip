a5eabfb4acc762551bb86659e63670c4c3940773
lmhufnagel <lmhufnagel@gmail.com>
2018-03-11 15:42:23 -0400

    cleanup model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,5 @@ class User < ApplicationRecord
   has_many :trips, foreign_key: :driver_id
   has_many :reviews_written, class_name: "Review", foreign_key: :reviewer
   has_many :reviews_received, class_name: "Review", foreign_key: :reviewee
-  has_many :rides, class_name: "Trip", through: :seats
 
 end
