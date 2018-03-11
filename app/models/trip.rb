828a4f65db7f89f45dd8d7fc7c47fda734278916
lmhufnagel <lmhufnagel@gmail.com>
2018-03-11 15:41:20 -0400

    cleanup model

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,7 +1,6 @@
 class Trip < ApplicationRecord
   belongs_to :driver, class_name: "User"
   has_many :seats, foreign_key: :ride_id
-  has_many :riders, class_name: "User", through: :seats
   has_many :reviews
 
 
